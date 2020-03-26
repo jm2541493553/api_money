cfc65e3371190367d92815b736c277601530b1b1
jm2541493553 <2541493553@qq.com>
2020-03-26 12:02:46 +0800

    个人中心

--- src/components/page/BaseForm.vue
+++ src/components/page/BaseForm.vue
@@ -1,37 +1,102 @@
 <template>
-    <div>
-        <div class="crumbs">
-            <el-breadcrumb separator="/">
-                <el-breadcrumb-item>
-                    <i class="el-icon-lx-calendar"></i> 表单
-                </el-breadcrumb-item>
-                <el-breadcrumb-item>基本表单</el-breadcrumb-item>
-            </el-breadcrumb>
-        </div>
-        <div class="container">
-            <div class="form-box">
-                <el-form ref="form" :model="form" label-width="100px">
-                    <el-form-item label="表单名称">
-                        <el-input v-model="form.name"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="onSubmit">表单提交</el-button>
-                        <el-button>取消</el-button>
-                    </el-form-item>
-                </el-form>
-            </div>
-        </div>
+    <div style="width:500px">
+        <el-form
+            :model="ruleForm"
+            :rules="rules"
+            ref="ruleForm"
+            label-width="100px"
+            class="demo-ruleForm"
+            label-position="left"
+        >
+            <el-form-item label="认购人账号" prop="num">
+                <el-input v-model="ruleForm.num"></el-input>
+            </el-form-item>
+            <el-form-item label="认购姓名" prop="name">
+                <el-input v-model="ruleForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="认购人性别" prop="sex">
+                <el-radio-group v-model="ruleForm.sex">
+                    <el-radio label="男"></el-radio>
+                    <el-radio label="女"></el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="电话号码" prop="phone">
+                <el-input v-model="ruleForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="身份证号" prop="card">
+                <el-input v-model="ruleForm.card"></el-input>
+            </el-form-item>
+            <el-form-item label="开户银行" prop="band">
+                <el-input v-model="ruleForm.band"></el-input>
+            </el-form-item>
+            <el-form-item label="银行卡号" prop="band_card">
+                <el-input v-model="ruleForm.band_card"></el-input>
+            </el-form-item>
+            <el-form-item label="支付宝号码" prop="zfb_num">
+                <el-input v-model="ruleForm.zfb_num"></el-input>
+            </el-form-item>
+            <el-form-item label="联系地址" prop="adress">
+                <el-input v-model="ruleForm.adress"></el-input>
+            </el-form-item>
+            <el-form-item prop="email"  label="邮箱" >
+                <el-input v-model="ruleForm.email"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm')">提交信息</el-button>
+                <el-button @click="resetForm('ruleForm')">重置</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
-
 <script>
 export default {
-    name: 'baseform',
     data() {
+        return {
+            ruleForm: {
+                num: '',
+                name: '',
+                sex: '',
+                phone: '',
+                card: '',
+                band: '',
+                band_card: '',
+                zfb_num: '',
+                adress: '',
+                email:''
+            },
+            rules: {
+                num: [{ required: true, message: '请输入认购人账号', trigger: 'blur' }],
+                name: [
+                    { required: true, message: '请输入认购人姓名', trigger: 'blur' },
+                    { min: 3, max: 5, message: '长度在 2 到 5 个字符', trigger: 'blur' }
+                ],
+                sex: [{ required: true, message: '请选择性别', trigger: 'change' }],
+                phone: [{ required: true, message: '请输入电话号码', trigger: 'blur' }],
+                card: [{ required: true, message: '请输入身份证号', trigger: 'blur' }],
+                band: [{ required: true, message: '请输入开户银行', trigger: 'blur' }],
+                band_card: [{ required: true, message: '请输入银行卡号', trigger: 'blur' }],
+                zfb_num: [{ required: true, message: '请输入支付宝号码', trigger: 'blur' }],
+                adress: [{ required: true, message: '请输入联系地址', trigger: 'blur' }],
+                email:[
+                    { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+                    { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+                ]
+            }
+        };
     },
     methods: {
-        onSubmit() {
-            this.$message.success('提交成功！');
+        submitForm(formName) {
+            this.$refs[formName].validate(valid => {
+                if (valid) {
+                    alert('submit!');
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
         }
     }
 };
--- src/components/page/Permission.vue
+++ src/components/page/Permission.vue
@@ -1,38 +1,251 @@
 <template>
     <div>
-        <div class="crumbs">
-            <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-lx-warn"></i> 信息修改</el-breadcrumb-item>
-            </el-breadcrumb>
-        </div>
-        <div class="container">
-            <h1>管理员权限页面</h1>
-            <p>只有用 admin 账号登录的才拥有管理员权限，才能进到这个页面，其他账号想进来都会跳到403页面，重新用管理员账号登录才有权限。</p>
-            <p>想尝试一下，请<router-link to="/login" class="logout">退出登录</router-link>，随便输入个账号名，再进来试试看。</p>
-        </div>
-
+        <el-row :gutter="20">
+            <el-col :span="8">
+                <el-card shadow="hover" class="mgb20" style="height:252px;">
+                    <div class="user-info">
+                        <img src="../../assets/img/img.jpg" class="user-avator" alt />
+                        <div class="user-info-cont">
+                            <div>{{role}}</div>
+                        </div>
+                    </div>
+                    <div class="user-info-list">
+                        上次登录时间：
+                        <span>2019-11-01</span>
+                    </div>
+                    <div class="user-info-list">
+                        上次登录地点：
+                        <span>东莞</span>
+                    </div>
+                </el-card>
+                <el-button type="warning" @click="dialogVisible = true">修改密码</el-button>
+                <el-dialog
+                    title="提示"
+                    :visible.sync="dialogVisible"
+                    width="30%"
+                    :before-close="handleClose"
+                >
+                    <span>
+                        <el-form
+                            :model="ruleForm"
+                            status-icon
+                            :rules="rules"
+                            ref="ruleForm"
+                            label-width="100px"
+                            class="demo-ruleForm"
+                        >
+                            <el-form-item label="密码" prop="pass">
+                                <el-input
+                                    type="password"
+                                    v-model="ruleForm.pass"
+                                    autocomplete="off"
+                                ></el-input>
+                            </el-form-item>
+                            <el-form-item label="确认密码" prop="checkPass">
+                                <el-input
+                                    type="password"
+                                    v-model="ruleForm.checkPass"
+                                    autocomplete="off"
+                                ></el-input>
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                                <el-button @click="resetForm('ruleForm')">重置</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </span>
+                </el-dialog>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
-    export default {
-        data: function(){
-            return {}
+import Schart from 'vue-schart';
+import bus from '../common/bus';
+export default {
+    name: 'dashboard',
+
+    data() {
+        var validatePass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请输入密码'));
+            } else {
+                if (this.ruleForm.checkPass !== '') {
+                    this.$refs.ruleForm.validateField('checkPass');
+                }
+                callback();
+            }
+        };
+        var validatePass2 = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请再次输入密码'));
+            } else if (value !== this.ruleForm.pass) {
+                callback(new Error('两次输入密码不一致!'));
+            } else {
+                callback();
+            }
+        };
+        return {
+            dialogVisible: false,
+            ruleForm: {
+                pass: '',
+                checkPass: ''
+            },
+            rules: {
+                pass: [{ validator: validatePass, trigger: 'blur' }],
+                checkPass: [{ validator: validatePass2, trigger: 'blur' }]
+            }
+        };
+    },
+    components: {
+        Schart
+    },
+    computed: {
+        role() {
+            return this.name === 'admin' ? '超级管理员' : '普通用户';
+        }
+    },
+    methods: {
+        submitForm(formName) {
+            this.$refs[formName].validate(valid => {
+                if (valid) {
+                    alert('submit!');
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+        },
+        handleClose(done) {
+            this.$confirm('确认关闭？')
+                .then(_ => {
+                    done();
+                })
+                .catch(_ => {});
+        },
+        changeDate() {
+            const now = new Date().getTime();
+            this.data.forEach((item, index) => {
+                const date = new Date(now - (6 - index) * 86400000);
+                item.name = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+            });
         }
     }
+};
 </script>
 
+
 <style scoped>
-h1{
+.el-row {
+    margin-bottom: 20px;
+}
+
+.grid-content {
+    display: flex;
+    align-items: center;
+    height: 100px;
+}
+
+.grid-cont-right {
+    flex: 1;
     text-align: center;
-    margin: 30px 0;
+    font-size: 14px;
+    color: #999;
 }
-p{
-    line-height: 30px;
-    margin-bottom: 10px;
-    text-indent: 2em;
+
+.grid-num {
+    font-size: 30px;
+    font-weight: bold;
 }
-.logout{
-    color: #409EFF;
+
+.grid-con-icon {
+    font-size: 50px;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    line-height: 100px;
+    color: #fff;
+}
+
+.grid-con-1 .grid-con-icon {
+    background: rgb(45, 140, 240);
+}
+
+.grid-con-1 .grid-num {
+    color: rgb(45, 140, 240);
+}
+
+.grid-con-2 .grid-con-icon {
+    background: rgb(100, 213, 114);
+}
+
+.grid-con-2 .grid-num {
+    color: rgb(45, 140, 240);
+}
+
+.grid-con-3 .grid-con-icon {
+    background: rgb(242, 94, 67);
+}
+
+.grid-con-3 .grid-num {
+    color: rgb(242, 94, 67);
+}
+
+.user-info {
+    display: flex;
+    align-items: center;
+    padding-bottom: 20px;
+    border-bottom: 2px solid #ccc;
+    margin-bottom: 20px;
+}
+
+.user-avator {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+}
+
+.user-info-cont {
+    padding-left: 50px;
+    flex: 1;
+    font-size: 14px;
+    color: #999;
+}
+
+.user-info-cont div:first-child {
+    font-size: 30px;
+    color: #222;
+}
+
+.user-info-list {
+    font-size: 14px;
+    color: #999;
+    line-height: 25px;
+}
+
+.user-info-list span {
+    margin-left: 70px;
+}
+
+.mgb20 {
+    margin-bottom: 20px;
+}
+
+.todo-item {
+    font-size: 14px;
+}
+
+.todo-item-del {
+    text-decoration: line-through;
+    color: #999;
+}
+
+.schart {
+    width: 100%;
+    height: 300px;
 }
 </style>
